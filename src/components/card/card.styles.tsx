79ac1a56d6daf759234743d39affc124a6f16a33
bitcrushedbark <35928509+BitcrushedBark@users.noreply.github.com>
2023-04-04 02:53:49 +0400

    fix: correct animation syntax

--- src/components/card/card.styles.tsx
+++ src/components/card/card.styles.tsx
@@ -78,12 +78,12 @@ export const Card = styled.form<CardProps & GenericStyleProps>`
 
   ${({ isMobile }) => isMobile ? 'scroll-margin-top: 30px;' : ''}
 
-  ${({ isActive, isMobile }) => isActive && css`
+  ${({ isActive, isMobile }) => isActive && `
     transform: scale(1.1);
     background-color: #1d689e;
     border-color: #1d689e;
     outline-color: #268bd2;
-    ${isMobile ? '' : `animation: ${grow} .5s 1;`};
+    ${isMobile ? '' : css`animation: ${grow} .5s 1;`};
   `}
 
   ${({ isCorrect }) => isCorrect && css`
@@ -91,8 +91,8 @@ export const Card = styled.form<CardProps & GenericStyleProps>`
     border-color: #80c9a9;
   `}
 
-  ${({ isWrong, isMobile }) => isWrong && css`
-    ${isMobile ? '' : `animation: ${errorShake} 1s linear 1;`};
+  ${({ isWrong, isMobile }) => isWrong && `
+    ${isMobile ? '' : css`animation: ${errorShake} 1s linear 1;`};
     background-color: #5783a2;
     border-color: #5783a2;
   `}
